99e0c89dd845ef933b1b420a21438b3dc3f469fb
RedowanSajeeb <redowansajeeb72@gmail.com>
2023-05-20 22:13:18 +0600

    get

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -8,9 +9,6 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = `mongodb+srv://${process.env.SECRET_user}:${process.env.SECRET_KEY}@brainytoy.uw3gtqh.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,14 +19,30 @@ const client = new MongoClient(uri, {
     deprecationErrors: true,
   },
 });
-
+  const brainYToyCollection = client.db("BrainYT").collection("StoreDB");
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+  // brainYToys Get in Server
+  app.get("/brainy", async (req, res) =>{
+     const cursor = brainYToyCollection.find()
+     const result = await cursor.toArray();
+     res.send(result);
+      
+  });
 
+  //  brainYToys Server Added
+   app.post("/brainy", async (req, res) => {
 
+    const brainYToys = req.body;
+    // console.log(brainYToys);
+   
+    const result = await brainYToyCollection.insertOne(brainYToys);
+    res.send(result);
+ 
+   });
     
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
